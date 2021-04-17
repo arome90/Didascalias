55cea1c57557ca3f33c98cf7d8832587697700ba
danilo08 <danilo08@ucm.es>
2021-04-17 15:06:44 +0200

    VR menu principal funcionando

--- ClassroomVR/Assets/Scripts/Managers/MenuManager.cs
+++ ClassroomVR/Assets/Scripts/Managers/MenuManager.cs
@@ -10,6 +10,7 @@ namespace ClassRoomVR
         //public GameObject PlayAndQuit_Obj; //Objeto "MenuPrincipal"
         public GameObject PackTriade_Obj;  //Objeto "PackMenu"
         public GameObject packB; //Objetos PackI
+        public GameObject packBVR; //Objetos PackIpara VR
         public GameObject PackTriade_ObjVR;
         public GameObject cameraRig;
 
@@ -37,16 +38,36 @@ namespace ClassRoomVR
         //El unico problema es que los 3 botones de pack se crean con scala 10000
         //Y en la z -99999 , por lo demas va perfect
         private void createPackButton(int i) {
-            GameObject a = Instantiate(packB);
-            a.name = i.ToString();
-            a.GetComponent<Button>().onClick.AddListener(delegate { PackButton(i); });
-            TextMeshProUGUI text = a.GetComponentInChildren<TextMeshProUGUI>();
-            string index = (i+1).ToString();
-            text.text = "Pack" + index;
-            a.transform.localScale = new Vector3(1, 1, 1);//no va
-            a.gameObject.transform.localScale = new Vector3(1, 1, 1);
-            if(!GameManager.Instance.getVR())a.transform.parent = PackTriade_Obj.transform;
-            else a.transform.parent = PackTriade_ObjVR.transform;
+
+            //diferenciar entre VR y no VR
+
+            GameObject a;
+            if (!GameManager.Instance.getVR())
+            {
+                a = Instantiate(packB);
+                a.name = i.ToString();
+                a.GetComponent<Button>().onClick.AddListener(delegate { PackButton(i); });
+                TextMeshProUGUI text = a.GetComponentInChildren<TextMeshProUGUI>();
+                string index = (i + 1).ToString();
+                text.text = "Pack" + index;
+                a.transform.localScale = new Vector3(1, 1, 1);
+                a.gameObject.transform.localScale = new Vector3(1, 1, 1);
+                a.transform.parent = PackTriade_Obj.transform;
+            }
+            else
+            {
+                a = Instantiate(packBVR);
+                a.transform.parent = PackTriade_ObjVR.transform;
+                a.gameObject.transform.position = new Vector3(0, 0, 0);
+                a.name = i.ToString();
+                a.GetComponent<Button>().onClick.AddListener(delegate { PackButton(i); });
+                TextMeshProUGUI text = a.GetComponentInChildren<TextMeshProUGUI>();
+                string index = (i + 1).ToString();
+                text.text = "Pack" + index;
+                a.transform.localScale = new Vector3(1, 1, 1);
+                a.gameObject.transform.localScale = new Vector3(1, 1, 1);
+              
+            }
         }
     }
 }
