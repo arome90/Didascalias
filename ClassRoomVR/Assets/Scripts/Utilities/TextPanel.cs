72c1b87e85d361fbd5c0cfe568214c48fb82e05c
Nanxi Qin <99988933+NanxiQin@users.noreply.github.com>
2024-12-30 20:01:07 +0100

    ajustes

--- ClassRoomVR/Assets/Scripts/Utilities/TextPanel.cs
+++ ClassRoomVR/Assets/Scripts/Utilities/TextPanel.cs
@@ -3,55 +3,57 @@ using TMPro;
 using UnityEngine.Events;
 using System;
 using System.Collections.Generic;
-
-public class StringWrapper
+namespace ClassRoomVR
 {
-    public string Value;
-}
-
-public class TextPanel : MonoBehaviour
-{
-    [System.Serializable]
-    public class MethodInfoWrapper
+    public class StringWrapper
     {
-        public string methodName;
-        public bool newLine;
-        public UnityEvent<StringWrapper> callbackEvent;
-    }
-    public List<MethodInfoWrapper> methods = new List<MethodInfoWrapper>();
-
-    public TextMeshProUGUI textMeshPro;
-
-    private void Start()
-    {
-        UpdateTextPanel();
+        public string Value;
     }
 
-    private void Update()
+    public class TextPanel : MonoBehaviour
     {
-        UpdateTextPanel();
-    }
-
-    public void UpdateTextPanel()
-    {
-        textMeshPro.text = string.Empty;
-
-        foreach (var methodInfo in methods)
+        [System.Serializable]
+        public class MethodInfoWrapper
         {
-            StringWrapper result = new StringWrapper();
-            try
-            {
-                methodInfo.callbackEvent.Invoke(result);
-                if(!string.IsNullOrEmpty(methodInfo.methodName)) textMeshPro.text += methodInfo.methodName;
-                textMeshPro.text += result.Value;
-                if (methodInfo.newLine) textMeshPro.text += "\n";
+            public string methodName;
+            public bool newLine;
+            public UnityEvent<StringWrapper> callbackEvent;
+        }
+        public List<MethodInfoWrapper> methods = new List<MethodInfoWrapper>();
 
-            }
-            catch (Exception e)
-            {
-                Debug.LogError($"Error invoking method {methodInfo.methodName}: {e.Message}");
-            }
+        public TextMeshProUGUI textMeshPro;
+
+        private void Start()
+        {
+            UpdateTextPanel();
         }
 
+        private void Update()
+        {
+            UpdateTextPanel();
+        }
+
+        public void UpdateTextPanel()
+        {
+            textMeshPro.text = string.Empty;
+
+            foreach (var methodInfo in methods)
+            {
+                StringWrapper result = new StringWrapper();
+                try
+                {
+                    methodInfo.callbackEvent.Invoke(result);
+                    if (!string.IsNullOrEmpty(methodInfo.methodName)) textMeshPro.text += methodInfo.methodName;
+                    textMeshPro.text += result.Value;
+                    if (methodInfo.newLine) textMeshPro.text += "\n";
+
+                }
+                catch (Exception e)
+                {
+                    Debug.LogError($"Error invoking method {methodInfo.methodName}: {e.Message}");
+                }
+            }
+
+        }
     }
 }
