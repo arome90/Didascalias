823e86b3fb741fc48cd5372583383dcd0da83699
NanxiQin <krystal12041994@gmail.com>
2025-02-11 21:29:44 +0100

    ajustes fuerza externa afecta a emoción

--- ClassRoomVR/Assets/Scripts/Managers/ExternalForceManager.cs
+++ ClassRoomVR/Assets/Scripts/Managers/ExternalForceManager.cs
@@ -145,8 +145,8 @@ public class ExternalForceManager : GenericSingleton<ExternalForceManager>
                             float impactValue = emotionImpact.Value;
 
                             // Modificar la emoción del estudiante
-                            float currentValue = studentEmotion.GetEmotionValue(emotionType);
-                            studentEmotion.SetEmotionValue(emotionType, currentValue + impactValue);
+                         
+                            student.ModifyEmotion(emotionType,impactValue);
                         }
                     }
                 }
