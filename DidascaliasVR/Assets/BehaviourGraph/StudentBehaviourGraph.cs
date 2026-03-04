e29113f38b43f7378fe59027ca4c6a82cf9a4cd5
Chicho <chichord2005@hotmail.com>
2026-03-04 14:58:06 +0100

    add graph toolkit and template files

---
+++ DidascaliasVR/Assets/BehaviourGraph/StudentBehaviourGraph.cs
@@ -0,0 +1,22 @@
+using System;
+using Unity.GraphToolkit.Editor;
+using UnityEditor;
+
+namespace Didascalia.BehaviourGraph
+{
+    [Graph(AssetExtension)]
+    [Serializable]
+    internal class StudentBehaviourGraph : Graph
+    {
+        internal const string AssetExtension = "dsbg";
+        internal const string CreateMenuPath = "Assets/Create/Graph/Student Behaviour Graph";
+
+        [MenuItem(CreateMenuPath, false)]
+        internal static void CreateAssetFile()
+        {
+            GraphDatabase.PromptInProjectBrowserToCreateNewAsset<StudentBehaviourGraph>();
+        }
+    }
+
+    // TODO: choose a matching representation for graph nodes and transitions. Talk with @DavidRainder
+}
