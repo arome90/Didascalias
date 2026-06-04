b44f6f70675136135f78ec56924fc2d415726be3
Chicho <chichord2005@hotmail.com>
2026-06-04 11:58:05 +0200

    fix OnStandUp state pulls behaviour from parent

--- DidascaliasVR/Assets/OnStandUp.cs
+++ DidascaliasVR/Assets/OnStandUp.cs
@@ -9,7 +9,7 @@ public class OnStandUp : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        StudentBehaviour behaviour = animator.GetComponent<StudentBehaviour>();
+        StudentBehaviour behaviour = animator.GetComponentInParent<StudentBehaviour>();
         Didascalia.Utils.Log.Info("OnStandUp: " + behaviour.name, this);
         behaviour.OnStandUp.Invoke();
     }
