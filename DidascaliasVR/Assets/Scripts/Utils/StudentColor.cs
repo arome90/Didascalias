702747ebf8cbcc5a32bab951eef8b942d005c715
Andrés Carnicero <62719876+AndresCarnicero@users.noreply.github.com>
2026-09-16 17:16:16 +0200

    Cambios gpu instancing para alumnos

---
+++ DidascaliasVR/Assets/Scripts/Utils/StudentColor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class StudentColor : MonoBehaviour
+{
+    [SerializeField]
+    Color[] hairColors;
+
+
+
+
+    [SerializeField] private Renderer studentRenderer;
+
+
+    private static readonly int ColorBagID =
+        Shader.PropertyToID("_Color_Hair");
+
+
+
+    void Start()
+    {
+        // A PARA NO CREAR INSTANCIAS DEL MATERIAL // B CREA DIFERENTES INSTANCIAS -> DA UN POCO IGUAL DE RENDIMIENTO
+        a();
+        //b();
+    }
+
+    private Color GetHairRandomColor()
+    {
+        return hairColors[Random.Range(0, hairColors.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void a()
+    {
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+
+        // ───── MOCHILA ─────
+
+        studentRenderer.GetPropertyBlock(block);
+
+        Color bagColor = GetHairRandomColor();
+
+        block.SetColor(ColorBagID, bagColor);
+
+        studentRenderer.SetPropertyBlock(block);
+
+    }
+
+    void b()
+    {
+        studentRenderer.material.SetColor("_Color_Hair", GetHairRandomColor());
+    }
+
+}
